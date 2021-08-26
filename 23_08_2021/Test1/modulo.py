b99a89b2b70a6d2683923f5419d289b736225a9c
taniagdn <31165833+taniagdn@users.noreply.github.com>
2021-08-25 23:18:18 -0500

    Add files via upload

--- 23_08_2021/Test1/modulo.py
+++ 23_08_2021/Test1/modulo.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Aug 23 09:24:51 2021
+Created on Mon Aug 23 09:24:29 2021
 
 @author: taniagualli
 """
 
 
-import modulo
+print("Soy un módulo")
