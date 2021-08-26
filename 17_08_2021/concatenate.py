18a3a3c199387f2b86174ace08685565cd5c1204
taniagdn <31165833+taniagdn@users.noreply.github.com>
2021-08-25 23:47:32 -0500

    Add files via upload

---
+++ 17_08_2021/concatenate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 
+
+@author: taniagualli
+"""
+
+#Concatenate
+
+str1="Cisco"
+str2="Networking"
+str3="Academy"
+space=" "
+print(str1+space+str2+space+str3)
---
+++ 17_08_2021/convertdatatypes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 
+
+@author: taniagualli
+"""
+
+#Converting Data Types
+
+
+x=3
+print("The value of x is " + x)
+
+print("This value of X is " + x)
+
+print("The value of x is " + str(x))
+
+type(x)
+x=str(x)
+type(x)
+
+
+
+
+pi = 22/7
+print(pi)
+
+
+print("{:.2f}".format(pi))
