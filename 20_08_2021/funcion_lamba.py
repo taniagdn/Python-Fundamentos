853b18171aa5ec1ae7c4ee2df0ba2ab5cc553d65
taniagdn <31165833+taniagdn@users.noreply.github.com>
2021-08-26 16:13:23 -0500

    Add files via upload

---
+++ 20_08_2021/funcion_lamba.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 20 23:39:51 2021
+
+@author: taniagualli
+"""
+
+
+a=[0, 1, -1, -2, 3, -4, 5, 6, 7]
+b=list(filter(lambda x: x>0, a))
+b
+
+print(b)
